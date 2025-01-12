e1190cc2b58fb9f5e8796a6b7996a25fd79d1803
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2025-01-12 17:51:54 +0530

    using the input provided by the users here in the defaultValue and defalutChecked from enteredValues like this: formState.enteredValues?.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -39,7 +39,16 @@ export default function Signup() {
     }
 
     if (errors.length > 0){
-      return { errors };
+      return { errors, enteredValues: {
+        email,
+        password,
+        confirmPassword,
+        firstName,
+        lastName,
+        role,
+        acquisitionChannel,
+        terms,
+      } };
     }
 
     return{ errors: null }
@@ -55,13 +64,14 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" defaultValue={formState.enteredValues?.email}/>
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password"
+          defaultValue={formState.enteredValues?.password} />
         </div>
 
         <div className="control">
@@ -70,6 +80,7 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            defaultValue={formState.enteredValue?.confirmPassword}
           />
         </div>
       </div>
@@ -79,18 +90,22 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name" 
+            defaultValue={formState.enteredValue?.firstName}
+          />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" 
+            defaultValue={formState.enteredValue?.lastName}
+          />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select id="role" name="role" defaultValue={formState.enteredValue?.role}>
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -107,6 +122,7 @@ export default function Signup() {
             id="google"
             name="acquisition"
             value="google"
+            defaultChecked = {formState.enteredValue?.acquisitionChannel.includes('google')}
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -117,19 +133,22 @@ export default function Signup() {
             id="friend"
             name="acquisition"
             value="friend"
+            defaultChecked = {formState.enteredValue?.acquisitionChannel.includes('friend')}
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input type="checkbox" id="other" name="acquisition" value="other" defaultChecked = {formState.enteredValue?.acquisitionChannel.includes('other')}/>
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          <input type="checkbox" id="terms-and-conditions" name="terms"  
+            defaultChecked={formState.enteredValue?.terms}
+          />I
           agree to the terms and conditions
         </label>
       </div>
